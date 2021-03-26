eccac9d33b95642c8ab9b87a13befe6df71c9fed
Malachi <malachialusa@gmail.com>
2021-03-25 19:23:16 -0600

    reg service worker

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,11 +1,74 @@
 let db;
+const request = indexedDB.open('budget', 1);
 
-const request = indexedDB.open('hearsaydb', 1);
-
-//if database changes
 request.onupgradeneeded = function(event) {
-    // save a reference to the database 
-    const db = event.target.result;
-    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('User', { autoIncrement: true });
+  const db = event.target.result;
+  db.createObjectStore('new_budget', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above), save reference to db in global variable
+  db = event.target.result;
+
+  // check if app is online, if yes run checkDatabase() function to send all local db data to api
+  if (navigator.onLine) {
+    uploadbudget();
+  }
+};
+
+request.onerror = function(event) {
+  // log error here
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(['new_budget'], 'readwrite');
+
+  const pizzaObjectStore = transaction.objectStore('new_budget');
+
+  // add record to your store with add method.
+  pizzaObjectStore.add(record);
+}
+
+function uploadbudget() {
+  // open a transaction on your pending db
+  const transaction = db.transaction(['new_budget'], 'readwrite');
+
+  // access your pending object store
+  const pizzaObjectStore = transaction.objectStore('new_budget');
+
+  // get all records from store and set to a variable
+  const getAll = pizzaObjectStore.getAll();
+
+  getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/pizzas', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(['new_budget'], 'readwrite');
+          const pizzaObjectStore = transaction.objectStore('new_budget');
+          // clear all items in your store
+          pizzaObjectStore.clear();
+        })
+        .catch(err => {
+          // set reference to redirect back here
+          console.log(err);
+        });
+    }
   };
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadbudget);

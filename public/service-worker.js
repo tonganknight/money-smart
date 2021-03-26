e39440fe67647ce9a9401aa1522b70435345839d
Malachi <malachialusa@gmail.com>
2021-03-26 10:05:05 -0600

    adjusted pathing, moved service worker to html index level

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,11 +3,11 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE =[
-    "../index.html",
-    "../css/style.css",
-    "./idb.js",
-    "./index.js",
-    "../../routes/api.js"
+    "./index.html",
+    "./css/style.css",
+    "./js/idb.js",
+    "./js/index.js",
+    "../routes/api.js"
 ];
 
 

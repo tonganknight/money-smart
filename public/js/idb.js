0574b18112181ba3246229a925d250c7f39ac2a6
Malachi <malachialusa@gmail.com>
2021-03-26 13:22:08 -0600

    I changed transaction to transactions throught the project to match my mongodb indexes

--- public/js/idb.js
+++ public/js/idb.js
@@ -43,7 +43,7 @@ function uploadbudget() {
   getAll.onsuccess = function() {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch('/api/pizzas', {
+      fetch('/api/transactions', {
         method: 'POST',
         body: JSON.stringify(getAll.result),
         headers: {
--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 let transactions = [];
 let myChart;
 
-fetch("/api/transaction")
+fetch("/api/transactions")
   .then(response => {
     return response.json();
   })
@@ -114,7 +114,7 @@ function sendTransaction(isAdding) {
   populateTotal();
   
   // also send to server
-  fetch("/api/transaction", {
+  fetch("/api/transactions", {
     method: "POST",
     body: JSON.stringify(transaction),
     headers: {
